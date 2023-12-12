e504576d7f4f0d28c8876c9dd3542f52dab4f464
10ish <75301244+10ish@users.noreply.github.com>
2023-12-12 18:10:46 +0530

    Update applicant.controller.js

--- controllers/applicant.controller.js
+++ controllers/applicant.controller.js
@@ -89,7 +89,7 @@ exports.login = async (req, res) => {
             httpOnly: true,
             path: '/',
             secure:true,
-            sameSite: false,
+            sameSite: 'none',
           
             maxAge: 1 * 24 * 60 * 60 * 1000 // 2 days in milliseconds
           });
